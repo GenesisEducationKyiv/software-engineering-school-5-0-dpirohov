78ef614a95f4d7642ce25b0e5bb0113b39023117
dpirohov <k3nz032@gmail.com>
2025-06-26 15:49:22 +0300

    hw5: Remove redundant files

--- internal/broker/event_bus.go
+++
@@ -1,9 +0,0 @@
-package broker
-
-import "context"
-
-type EventBusInterface interface {
-	Publish(topic Topic, payload []byte, opts ...PublishOption) error
-	Subscribe(ctx context.Context, topic Topic, handler func([]byte) error) error
-	Close() error
-}
